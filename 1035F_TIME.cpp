d93a6fc67797ca846eb241008c408a42b0995517
PriyanshuNamadutt <namaduttpriyanshu@gmail.com>
2025-07-08 23:30:50 +0530

    Create 1035F_TIME.cpp
    Time Limit Exceed

---
+++ 1035F_TIME.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int dfs( int node, vector<int> &lava, unordered_map<int,list<int>> &adj, int life, vector<int> &wt, int time ) {
+
+    life += wt[node];
+    if ( life == 0 || lava[node] <= time ) return 0;
+    int ans = 0;
+    for ( auto it : adj[node] ) {
+        ans = max( ans , 1 + dfs(it, lava, adj, life, wt, time+1) );
+    }
+    return ans;
+}
+
+void dfs1( int node, vector<int> &lava, unordered_map<int,list<int>> &adj, int time, vector<int> &vis ) {
+
+    vis[node] = 1;
+    lava[node] = time;
+
+    for ( auto it : adj[node] ) {
+        if ( !vis[it] ) {
+            dfs1( it, lava, adj, time+1, vis );
+        }
+    }
+}
+
+int main () {
+    int t; cin >> t;
+    while ( t-- ) {
+        int n, st;
+        cin >> n >> st;
+        st -= 1;
+        vector<int> wt(n);
+        for ( int i = 0; i < n; i++ ) {
+            cin >> wt[i];
+        }
+        unordered_map<int,list<int>> adj;
+        for ( int i = 0; i < n-1; i++ ) {
+            int u, v;
+            cin >> u >> v;
+            u-- , v--;
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> lava(n);
+        vector<int> vis( n, 0);
+        dfs1( 0, lava, adj, 0, vis );
+
+        int ans = dfs( st, lava, adj, 1, wt, 0 );
+        cout << ans << endl;
+    }
+    return 0;
+}
